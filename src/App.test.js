4db89109603473333ae527bf2465ad7fdbb98183
M V Anand <abhi555rram@gmail.com>
2026-02-25 10:14:42 +0530

    changed the resume

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders about me link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/ABOUT ME/i);
-  expect(linkElement).toBeInTheDocument();
-});
